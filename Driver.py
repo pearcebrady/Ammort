89d5ba6848bec769cd9fbb9d29d09b7ce1791e7f
Brady Pearce <pearcebrady@gmail.com>
2022-06-26 10:18:26 -0400

    remove info

--- Driver.py
+++ Driver.py
@@ -3,15 +3,10 @@ import Budget
 import Expenses
 import subprocess
 
-home = Loan.Loan("House", 327750, 2.625, 30)
 condo = Loan.Loan("Condo", 185000, 6.7, 20)
-car = Loan.Loan("Prius Prime", 36904.72, 2.5, 6)
 
 condo.add_one_time_payment(10000, 1)
 condo.add_recurring_payment(2000)
-
-total = Budget.Budget([home, condo, car])
-
 
 print(condo.find_zero())
 condo.export_ammort_table()
